ea12dcb2c4cb10e139cb39048ff2a8cb0f71ffef
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-09-09 15:46:40 -0400

    navbar link, styled-components add

--- src/NavBar/MenuItems.js
+++ src/NavBar/MenuItems.js
@@ -9,9 +9,4 @@ export const MenuItems = [
     url: "/QuantaVerse-Uniswap-Interface/liquidity",
     cName: "nav-links",
   },
-  {
-    title: "Verify ID",
-    url:   "https://app.fractal.id/authorize?client_id=4pVYR9NMGa1HE198Fe3VFS0IM5N2CrKCsrYO74Eg4uU&redirect_uri=https%3A%2F%2Fda-fi.io%2Fcallback&response_type=code&scope=contact%3Aread%20verification.basic%3Aread%20verification.basic.details%3Aread%20verification.liveness%3Aread%20verification.liveness.details%3Aread/",
-    cName: "nav-links",
-  },
 ];

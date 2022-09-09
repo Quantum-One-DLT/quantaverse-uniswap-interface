7b9842b16d5bf86fd7362a283abca64860ef5674
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-09-09 14:08:14 -0400

    rgb(3,255,246)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { ethers } from "ethers";
 import Web3Provider from "./network";
 import NavBar from "./NavBar/NavBar";
 import CoinSwapper from "./CoinSwapper/CoinSwapper";
-import { Route } from "react-router-dom";
+import { Route, Redirect, Link } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
 import Liquidity from "./Liquidity/Liquidity";
 import MenuItems from "./NavBar/MenuItems";
@@ -50,10 +50,7 @@ const App = () => {
                 <Route exact path="/QuantaVerse-Uniswap-Interface/liquidity">
                   <Liquidity network={network} />
                 </Route>
-                <Route 
-                  exact path="/QuantaVerse-Uniswap-Interface/Verify-id"
-                  component={() => {window.open('https://app.fractal.id/authorize?client_id=4pVYR9NMGa1HE198Fe3VFS0IM5N2CrKCsrYO74Eg4uU&redirect_uri=https%3A%2F%2Fda-fi.io%2Fcallback&response_type=code&scope=contact%3Aread%20verification.basic%3Aread%20verification.basic.details%3Aread%20verification.liveness%3Aread%20verification.liveness.details%3Aread')
-                  }}>
+                <Route>
                </Route>
               </div>
             )}
--- src/NavBar/MenuItems.js
+++ src/NavBar/MenuItems.js
@@ -11,7 +11,7 @@ export const MenuItems = [
   },
   {
     title: "Verify ID",
-    url: "/QuantaVerse-Uniswap-Interface/Verify-id",
+    url:   "https://app.fractal.id/authorize?client_id=4pVYR9NMGa1HE198Fe3VFS0IM5N2CrKCsrYO74Eg4uU&redirect_uri=https%3A%2F%2Fda-fi.io%2Fcallback&response_type=code&scope=contact%3Aread%20verification.basic%3Aread%20verification.basic.details%3Aread%20verification.liveness%3Aread%20verification.liveness.details%3Aread/",
     cName: "nav-links",
   },
 ];
